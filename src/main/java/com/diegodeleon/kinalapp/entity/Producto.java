4961fd7b1639555e94d4d82bb74988a65664649e
ddeleon-2022166 <ddeleon-2022166@kinal.edu.gt>
2026-03-25 17:38:51 -0600

    feat: Se agregó la entidad Productos

---
+++ src/main/java/com/diegodeleon/kinalapp/entity/Producto.java
@@ -0,0 +1,56 @@
+package com.diegodeleon.kinalapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//a
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @Column(name = "codigo_producto")
+    private int codigoProducto;
+    @Column
+    private String nombreProducto;
+    @Column
+    private double precio;
+    @Column
+    private int stock;
+    @Column
+    private int estado;
+
+    public Producto(){
+    }
+
+    public Producto(Integer codigoProducto, String nombreProducto, double precio, Integer stock, Integer estado) {
+        this.codigoProducto = codigoProducto;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public Integer getCodigoProducto() {return codigoProducto; }
+
+    public void setCodigoProducto(Integer codigoProducto) { this.codigoProducto = codigoProducto; }
+
+    public String getNombreProducto() { return nombreProducto; }
+
+    public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
+
+    public double getPrecio() { return precio; }
+
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    public Integer getStock() { return stock; }
+
+    public void setStock(Integer stock) { this.stock = stock; }
+
+    public Integer getEstado() { return estado; }
+
+    public void setEstado(Integer estado) {
+        this.estado = estado;
+    }
+}
