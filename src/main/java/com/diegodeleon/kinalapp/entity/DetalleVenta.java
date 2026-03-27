ce2dabcb9277fffbe55421ab3dff984a0cd99512
ddeleon-2022166 <ddeleon-2022166@kinal.edu.gt>
2026-03-26 21:00:17 -0600

    feat: Se agregó la entidad DetalleVenta

---
+++ src/main/java/com/diegodeleon/kinalapp/entity/DetalleVenta.java
@@ -0,0 +1,66 @@
+package com.diegodeleon.kinalapp.entity;
+
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "detalle_venta")
+public class DetalleVenta {
+
+    @Id
+    @Column(name = "codigo_detalle_venta")
+    private int codigoDetalleVenta;
+
+    @Column
+    private int cantidad;
+
+    @Column(name = "precio_unitario")
+    private double precioUnitario;
+
+    @Column
+    private double subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "ventas_codigo_venta")
+    @JsonIgnore
+    private Venta venta;
+
+    @ManyToOne
+    @JoinColumn(name = "productos_codigo_producto")
+    private Producto producto;
+
+    public DetalleVenta() {}
+
+    public DetalleVenta(int codigoDetalleVenta, int cantidad, double precioUnitario, double subtotal, Venta venta, Producto producto) {
+        this.codigoDetalleVenta = codigoDetalleVenta;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+        this.venta = venta;
+        this.producto = producto;
+    }
+
+    public int getCodigoDetalleVenta() { return codigoDetalleVenta; }
+
+    public void setCodigoDetalleVenta(int codigoDetalleVenta) { this.codigoDetalleVenta = codigoDetalleVenta; }
+
+    public int getCantidad() { return cantidad; }
+
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+
+    public double getPrecioUnitario() { return precioUnitario; }
+
+    public void setPrecioUnitario(double precioUnitario) { this.precioUnitario = precioUnitario; }
+
+    public double getSubtotal() { return subtotal; }
+
+    public void setSubtotal(double subtotal) { this.subtotal = subtotal; }
+
+    public Venta getVenta() { return venta; }
+
+    public void setVenta(Venta venta) { this.venta = venta; }
+
+    public Producto getProducto() { return producto; }
+
+    public void setProducto(Producto producto) { this.producto = producto; }
+}
